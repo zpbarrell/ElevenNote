9c680271718e44998cf1052282d5491aeff763ce
Zack Barrell <zpbarrell@gmail.com>
2022-08-02 19:44:30 -0400

    Entity folder added to Data layer

---
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data.Entities
+{
+    public class UserEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
