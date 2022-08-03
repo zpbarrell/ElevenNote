2bf99f0f61b7d1ebc09d0e71f1f7d212f316caf6
Zack Barrell <zpbarrell@gmail.com>
2022-08-02 21:38:37 -0400

    UserController added

---
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElevenNote.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+    }
+}
