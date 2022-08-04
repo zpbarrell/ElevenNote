c87032bbd9dc559fba3e4262949b51a1aa8cf0d6
Zack Barrell <zpbarrell@gmail.com>
2022-08-04 17:43:45 -0400

    Password hashed

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User
@@ -25,11 +26,13 @@ namespace ElevenNote.Services.User
             { 
                 Email = model.Email,
                 Username = model.Username,
-                Password = model.Password,
                 FirstName = model.FirstName,
                 LastName = model.LastName,
                 DateCreated = DateTime.Now
             };
+
+            var passwordHasher = new PasswordHasher<UserEntity>();
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
 
             _context.Users.Add(entity);
             var numberOfChanges = await _context.SaveChangesAsync();
